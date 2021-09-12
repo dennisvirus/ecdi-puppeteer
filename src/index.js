42f69235b79926d5d4fff52f5464a2fc0ba99f73
dennisvirus <dennisvirus@gmail.com>
2021-09-12 16:32:03 +0900

    퍼피티어 스냅샷

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1280,
+        height: 1024
+    });
     await page.goto('https://www.11st.co.kr/products/2933183678');
     // await page.screenshot({ path: '/Users/dennis/screenshot2.png' }); //스크린샷찍기
     await page.screenshot({
         fullPage: true,
-        path: `/Users/dennis/screenshot-${new Date().toISOString().substr(0, 10)}.png`
+        path: `/Users/dennis/screenshot-${new Date().toISOString().substr(0, 19)}.png`
     })
     await browser.close(); //puppeteer 종료
 })();
