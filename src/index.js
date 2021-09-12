1cbce751054f8a524bb1ef462e2ad601cf217010
dennisvirus <dennisvirus@gmail.com>
2021-09-12 16:23:22 +0900

    퍼피티어

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://www.naver.com');
+    await page.goto('https://www.11st.co.kr/products/2933183678');
+    // await page.screenshot({ path: '/Users/dennis/screenshot2.png' }); //스크린샷찍기
+    await page.screenshot({
+        fullPage: true,
+        path: `/Users/dennis/screenshot-${new Date().toISOString().substr(0, 10)}.png`
+    })
+    await browser.close(); //puppeteer 종료
 })();
